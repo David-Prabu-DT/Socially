cc6936fb6f43123cd90542429061f6b4b7393b94
David Prabu <davidprabu162@gmail.com>
2022-09-07 08:53:50 +0530

    auth page

--- src/api/AuthRequests.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { logInType, signUpType } from "../types/Global";
-
-const API = axios.create({ baseURL: process.env.BASE_URL });
-
-export const logIn = (formData: logInType) => API.post("/auth/login", formData);
-
-export const signUp = (formData: signUpType) =>
-  API.post("/auth/register", formData);
