8d1d0a8d69c3309ffa1b7fa070d318ab2852d9b5
David Prabu <davidprabu@dorustree.in>
2022-09-12 19:31:45 +0530

    jwt added

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,17 +2,25 @@ import UserModel from "../models/userModel.js";
 
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+let token;
 // Get a User
 export const getUser = async (req, res) => {
   const id = req.params.id;
 
   try {
     const user = await UserModel.findById(id);
-    console.log("userdata",user);
+
     if (user) {
       const { password, ...otherDetails } = user._doc;
 
-      res.status(200).json(otherDetails);
+      token = jwt.sign(
+        { username: user.username, id: user._id },
+        process.env.JWT_KEY,
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).json({ otherDetails, token });
     } else {
       res.status(404).json("No such User");
     }
@@ -29,7 +37,8 @@ export const getAllUsers = async (req, res) => {
       const { password, ...otherDetails } = user._doc;
       return otherDetails;
     });
-    res.status(200).json(users);
+
+    res.status(200).json({ users, token });
   } catch (error) {
     res.status(500).json(error);
   }
